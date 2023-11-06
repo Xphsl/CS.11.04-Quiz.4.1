329342da10c848e6c2344e1c08710dc4c8e693cf
Xphsl <stevenl2026@student.cis.edu.hk>
2023-11-06 12:19:55 +0800

    Quiz 4.1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels (String a){
+        String a = str.length();
+        String vowelcount = "";
+        for(int i=0; i<str.length();i++){
+            char ab = str.charAt(i);
+            if(ab == 'a'|| ab == 'e'|| ab == 'i' || ab == 'o' || ab == 'u'){
+                return vowelcount++;
+            }
+
+        }
+
+    }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,7 +25,12 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
-
+    public static int notDivisibleBy235(int N){
+        int countV = 0;
+            for (int i=0; i<N; i++){
+                System.out.println();
+        }
+    }
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -24,6 +40,7 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
-
-
+    public static String camelCaseMe(String a){
+        System.out.println();
+    }
 }
